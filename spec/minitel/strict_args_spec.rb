b7f8337351a7b6155415cc0f46cc3f8913ed1ddf
Ben Fritsch <bfritsch@salesforce.com>
2026-04-30 17:10:21 +0200

    fix Layout/SpaceInsideParens

--- spec/minitel/strict_args_spec.rb
+++ spec/minitel/strict_args_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Minitel::StrictArgs, '.enforce' do
     end
 
     it 'fails if there is an extra key' do
-      @hash.merge!( { foo: 3 } )
+      @hash.merge!({ foo: 3 })
       expect { Minitel::StrictArgs.enforce(@hash, @required, @optional, :uuid) }.to raise_error(ArgumentError)
     end
   end
