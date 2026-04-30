55948905cfd8d7e6ecf1bbaf65d1026c76024959
Ben Fritsch <bfritsch@salesforce.com>
2026-04-30 17:35:46 +0200

    fix RSpec/InstanceVariable

--- spec/minitel/client_spec.rb
+++ spec/minitel/client_spec.rb
@@ -3,15 +3,12 @@
 require "spec_helper"
 
 RSpec.describe Minitel::Client, "#initialize" do
-  before do
-    url = "https://EXAMPLE-KEY0-0000-0000-000000000000:EXAMPLE-SEC0-0000-0000-000000000000@telex.example.com"
-    @client = described_class.new(url)
-  end
+  let(:client) { described_class.new("https://EXAMPLE-KEY0-0000-0000-000000000000:EXAMPLE-SEC0-0000-0000-000000000000@telex.example.com") }
 
   it "uses the given url and credentials" do
-    expect(@client.uri.host).to eq("telex.example.com")
-    expect(@client.user).to eq("EXAMPLE-KEY0-0000-0000-000000000000")
-    expect(@client.password).to eq("EXAMPLE-SEC0-0000-0000-000000000000")
+    expect(client.uri.host).to eq("telex.example.com")
+    expect(client.user).to eq("EXAMPLE-KEY0-0000-0000-000000000000")
+    expect(client.password).to eq("EXAMPLE-SEC0-0000-0000-000000000000")
   end
 
   it "requires an https url" do
@@ -23,9 +20,8 @@ end
 RSpec.describe Minitel::Client, "#notify_app" do
   let(:defaults) { { title: "a title", body: "a body", app_uuid: SecureRandom.uuid } }
   let(:client) { described_class.new("https://EXAMPLE-KEY0-0000-0000-000000000000:EXAMPLE-SEC0-0000-0000-000000000000@telex.example.com") }
-
-  before do
-    @stub = WebMock.stub_request(:post, "https://telex.example.com/producer/messages")
+  let!(:stub) do
+    WebMock.stub_request(:post, "https://telex.example.com/producer/messages")
       .to_return(status: 201, body: JSON.generate(success: true))
   end
 
@@ -36,17 +32,17 @@ RSpec.describe Minitel::Client, "#notify_app" do
       body: "a body",
       target: { type: "app", id: defaults[:app_uuid] },
     )
-    expect(@stub.with(body: body)).to have_been_requested
+    expect(stub.with(body: body)).to have_been_requested
   end
 
   it "sends basic auth credentials" do
     client.notify_app(defaults)
-    expect(@stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
+    expect(stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
   end
 
   it "sends correct content-type and user-agent headers" do
     client.notify_app(defaults)
-    expect(@stub.with(headers: {
+    expect(stub.with(headers: {
       "Content-Type" => "application/json",
       "User-Agent" => "minitel/#{Minitel::VERSION}",
     })).to have_been_requested
@@ -55,7 +51,7 @@ RSpec.describe Minitel::Client, "#notify_app" do
   it "supports actions" do
     action = { label: "omg", url: "https://foo" }
     client.notify_app(defaults.merge(action: action))
-    post_with_action = @stub.with do |req|
+    post_with_action = stub.with do |req|
       body = JSON.parse(req.body, symbolize_names: true)
       body[:action] == action
     end
@@ -116,9 +112,8 @@ end
 RSpec.describe Minitel::Client, "#notify_user" do
   let(:defaults) { { title: "a title", body: "a body", user_uuid: SecureRandom.uuid } }
   let(:client) { described_class.new("https://EXAMPLE-KEY0-0000-0000-000000000000:EXAMPLE-SEC0-0000-0000-000000000000@telex.example.com") }
-
-  before do
-    @stub = WebMock.stub_request(:post, "https://telex.example.com/producer/messages")
+  let!(:stub) do
+    WebMock.stub_request(:post, "https://telex.example.com/producer/messages")
       .to_return(status: 201, body: JSON.generate(success: true))
   end
 
@@ -129,17 +124,17 @@ RSpec.describe Minitel::Client, "#notify_user" do
       body: "a body",
       target: { type: "user", id: defaults[:user_uuid] },
     )
-    expect(@stub.with(body: body)).to have_been_requested
+    expect(stub.with(body: body)).to have_been_requested
   end
 
   it "sends basic auth credentials" do
     client.notify_user(defaults)
-    expect(@stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
+    expect(stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
   end
 
   it "sends correct content-type and user-agent headers" do
     client.notify_user(defaults)
-    expect(@stub.with(headers: {
+    expect(stub.with(headers: {
       "Content-Type" => "application/json",
       "User-Agent" => "minitel/#{Minitel::VERSION}",
     })).to have_been_requested
@@ -148,7 +143,7 @@ RSpec.describe Minitel::Client, "#notify_user" do
   it "supports actions" do
     action = { label: "omg", url: "https://foo" }
     client.notify_user(defaults.merge(action: action))
-    post_with_action = @stub.with do |req|
+    post_with_action = stub.with do |req|
       body = JSON.parse(req.body, symbolize_names: true)
       body[:action] == action
     end
@@ -195,25 +190,25 @@ RSpec.describe Minitel::Client, "#add_followup" do
   let(:defaults) { { body: "a body", message_uuid: SecureRandom.uuid } }
   let(:client) { described_class.new("https://EXAMPLE-KEY0-0000-0000-000000000000:EXAMPLE-SEC0-0000-0000-000000000000@telex.example.com") }
 
-  before do
-    @stub = WebMock.stub_request(:post, "https://telex.example.com/producer/messages/#{defaults[:message_uuid]}/followups")
+  let!(:stub) do
+    WebMock.stub_request(:post, "https://telex.example.com/producer/messages/#{defaults[:message_uuid]}/followups")
       .to_return(status: 201, body: JSON.generate(success: true))
   end
 
   it "posts a proper json body to the producer messages endpoint" do
     client.add_followup(defaults)
     body = JSON.generate(body: "a body")
-    expect(@stub.with(body: body)).to have_been_requested
+    expect(stub.with(body: body)).to have_been_requested
   end
 
   it "sends basic auth credentials" do
     client.add_followup(defaults)
-    expect(@stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
+    expect(stub.with(basic_auth: ["EXAMPLE-KEY0-0000-0000-000000000000", "EXAMPLE-SEC0-0000-0000-000000000000"])).to have_been_requested
   end
 
   it "sends correct content-type and user-agent headers" do
     client.add_followup(defaults)
-    expect(@stub.with(headers: {
+    expect(stub.with(headers: {
       "Content-Type" => "application/json",
       "User-Agent" => "minitel/#{Minitel::VERSION}",
     })).to have_been_requested
--- spec/minitel/strict_args_spec.rb
+++ spec/minitel/strict_args_spec.rb
@@ -4,39 +4,37 @@ require "spec_helper"
 
 RSpec.describe Minitel::StrictArgs, ".enforce" do
   describe "arguments" do
-    before do
-      @hash = { one: 1, two: 2, uuid: SecureRandom.uuid }
-      @required = [:one, :uuid]
-      @optional = [:two]
-    end
+    let(:input) { { one: 1, two: 2, uuid: SecureRandom.uuid } }
+    let(:required_keys) { [:one, :uuid] }
+    let(:optional_keys) { [:two] }
 
     it "works when all listed args are present" do
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.not_to raise_error
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.not_to raise_error
     end
 
     it "works when optional args are omitted" do
-      @hash.delete(:two)
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.not_to raise_error
+      input.delete(:two)
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.not_to raise_error
     end
 
     it "fails when a key is missing from the arg hash" do
-      @hash.delete(:one)
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.to raise_error(ArgumentError)
+      input.delete(:one)
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.to raise_error(ArgumentError)
     end
 
     it "fails when a key is nil" do
-      @hash[:one] = nil
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.to raise_error(ArgumentError)
+      input[:one] = nil
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.to raise_error(ArgumentError)
     end
 
     it "fails if the uuid column uuid is not a uuid" do
-      @hash[:uuid] = "not a uuid"
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.to raise_error(ArgumentError)
+      input[:uuid] = "not a uuid"
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.to raise_error(ArgumentError)
     end
 
     it "fails if there is an extra key" do
-      @hash[:foo] = 3
-      expect { described_class.enforce(@hash, @required, @optional, :uuid) }.to raise_error(ArgumentError)
+      input[:foo] = 3
+      expect { described_class.enforce(input, required_keys, optional_keys, :uuid) }.to raise_error(ArgumentError)
     end
   end
 end
