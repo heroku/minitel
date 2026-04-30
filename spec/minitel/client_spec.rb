9d5f7d65765a2fb306f809054d4a01ded4d31e51
Ben Fritsch <bfritsch@salesforce.com>
2026-04-30 17:10:57 +0200

    fix RSpec/NotToNot

--- spec/minitel/client_spec.rb
+++ spec/minitel/client_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Minitel::Client, '#initialize' do
 
   it 'requires an https url' do
     expect { Minitel::Client.new('http://user:pass@what.com') }.to raise_error(ArgumentError)
-    expect { Minitel::Client.new('https://user:pass@what.com') }.to_not raise_error
+    expect { Minitel::Client.new('https://user:pass@what.com') }.not_to raise_error
   end
 end
 
