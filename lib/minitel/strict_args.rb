4438cc2db69d96bfeef8ae25661ecf095dcb9a38
Ben Fritsch <bfritsch@salesforce.com>
2026-04-30 17:10:45 +0200

    fix Performance/RegexpMatch

--- lib/minitel/strict_args.rb
+++ lib/minitel/strict_args.rb
@@ -27,7 +27,7 @@ module Minitel
     end
 
     def ensure_is_uuid(uuid)
-      unless uuid =~ /\A[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}\z/i
+      unless /\A[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}\z/i.match?(uuid)
         raise ArgumentError, "'#{uuid.inspect}' not formated like a uuid"
       end
     end
